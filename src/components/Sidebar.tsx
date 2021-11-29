b1e9351116632eee263a9ed22570b33e808e2553
Lxk3y <47719567+Lxk3y@users.noreply.github.com>
2021-11-29 13:58:55 +0000

    Add modal support into 'new_project' Sidebar Project

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,26 @@
 import type { Project } from '@unify/types';
-import React from 'react';
+import React, { useState } from 'react';
 import { SidebarProject } from './SidebarProject';
+import { CreateProjectModal } from './CreateProjectModal';
 
 export type SidebarProps = {
     projects: Project[];
 };
 
 export const Sidebar: React.FC<SidebarProps> = ({ projects }) => {
+    const [modalState, setModalState] = useState<boolean>(false);
+
+    const handleModelOpen = (): void => {
+        setModalState((prevState) => !prevState);
+    };
+
     return (
         <div className="flex flex-col h-full p-2 bg-gray-100 rounded-tr-xl rounded-br-xl">
             {projects.map((project) => (
                 <SidebarProject type={'project'} key={project.id} {...project} />
             ))}
-            <SidebarProject type={'new_project'} key={'-1'} />
+            <SidebarProject type={'new_project'} key={'-1'} onClickProject={handleModelOpen} />
+            <CreateProjectModal modalOpen={modalState} handleModelOpen={handleModelOpen} />
         </div>
     );
 };
--- src/components/SidebarProject.tsx
+++ src/components/SidebarProject.tsx
@@ -2,9 +2,16 @@ import type { Project } from '@unify/types';
 import React from 'react';
 import { PlusIcon } from '@heroicons/react/solid';
 
-export const SidebarProject: React.FC<{ type: 'project' | 'new_project'; project?: Project }> = ({ type, project }) => {
+export const SidebarProject: React.FC<{
+    type: 'project' | 'new_project';
+    project?: Project;
+    onClickProject?: () => void;
+}> = ({ type, project, onClickProject }) => {
     return (
-        <div className="p-2 bg-gray-200 text-gray-500 rounded-lg transition-all hover:rounded-full hover:bg-gray-300 hover:text-gray-600">
+        <div
+            className="p-2 bg-gray-200 text-gray-500 rounded-lg transition-all hover:rounded-full hover:bg-gray-300 hover:text-gray-600"
+            onClick={onClickProject}
+        >
             {type == 'new_project' || project == null ? (
                 <PlusIcon className="w-8 h-8" />
             ) : (
